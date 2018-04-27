494dab16ca76a4f48544c57f84e3e04c1b67ed9e
Chris Shepard <csshepard@gmail.com>
2018-04-26 23:21:51 -0400

    Pulled sandwich info into reusable layout Changed activity_detail layout to linear layout Added landscape activity_detail Hiding Views for missing data Added error image when image is missing (https://upload.wikimedia.org/wikipedia/commons/4/44/Antu_emblem-unavailable.svg)

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,10 +18,7 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
-    private TextView descriptionTv;
-    private TextView alsoKnownAsTv;
-    private TextView originTv;
-    private TextView ingredientsTv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +27,16 @@ public class DetailActivity extends AppCompatActivity {
 
         ImageView ingredientsIv = findViewById(R.id.image_iv);
 
-        descriptionTv = findViewById(R.id.description_tv);
-        alsoKnownAsTv = findViewById(R.id.also_known_tv);
-        originTv = findViewById(R.id.origin_tv);
-        ingredientsTv = findViewById(R.id.ingredients_tv);
-
         Intent intent = getIntent();
+
+        int position = DEFAULT_POSITION;
+
         if (intent == null) {
             closeOnError();
+        } else {
+            position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         }
 
-        int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
             // EXTRA_POSITION not found in intent
             closeOnError();
@@ -56,8 +53,11 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         populateUI(sandwich);
+        // emblem_unavailable taken from
+        // https://upload.wikimedia.org/wikipedia/commons/4/44/Antu_emblem-unavailable.svg
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .error(R.drawable.emblem_unavailable)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -68,30 +68,49 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    // TODO: Hide Blank Fields
     private void populateUI(Sandwich sandwich) {
-        if (sandwich.getDescription() != null) {
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        TextView descriptionLabelTv = findViewById(R.id.description_label_tv);
+        TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        TextView alsoKnownAsLabelTv = findViewById(R.id.also_known_label_tv);
+        TextView originTv = findViewById(R.id.origin_tv);
+        TextView originLabelTv = findViewById(R.id.origin_label_tv);
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+        TextView ingredientsLabelTv = findViewById(R.id.ingredients_label_tv);
+        if (sandwich.getDescription() != null && !sandwich.getDescription().isEmpty()) {
             descriptionTv.setText(sandwich.getDescription());
+            descriptionLabelTv.setVisibility(View.VISIBLE);
+            descriptionTv.setVisibility(View.VISIBLE);
         } else {
-            descriptionTv.setText("");
+            descriptionLabelTv.setVisibility(View.GONE);
+            descriptionTv.setVisibility(View.GONE);
         }
 
-        if (sandwich.getPlaceOfOrigin() != null) {
+        if (sandwich.getPlaceOfOrigin() != null && !sandwich.getPlaceOfOrigin().isEmpty()) {
+            originLabelTv.setEnabled(true);
+            originTv.setEnabled(true);
             originTv.setText(sandwich.getPlaceOfOrigin());
         } else {
-            originTv.setText("");
+            originLabelTv.setVisibility(View.GONE);
+            originTv.setVisibility(View.GONE);
         }
 
-        if (sandwich.getIngredients() != null) {
+        if (sandwich.getIngredients() != null && !sandwich.getIngredients().isEmpty()) {
+            ingredientsLabelTv.setVisibility(View.VISIBLE);
+            ingredientsTv.setVisibility(View.VISIBLE);
             ingredientsTv.setText(TextUtils.join("\n", sandwich.getIngredients()));
         } else {
-            ingredientsTv.setText("");
+            ingredientsLabelTv.setVisibility(View.GONE);
+            ingredientsTv.setVisibility(View.GONE);
         }
 
-        if (sandwich.getAlsoKnownAs() != null) {
+        if (sandwich.getAlsoKnownAs() != null && !sandwich.getAlsoKnownAs().isEmpty()) {
+            alsoKnownAsLabelTv.setVisibility(View.VISIBLE);
+            alsoKnownAsTv.setVisibility(View.VISIBLE);
             alsoKnownAsTv.setText(TextUtils.join("\n", sandwich.getAlsoKnownAs()));
         } else {
-            ingredientsTv.setText("");
+            alsoKnownAsLabelTv.setVisibility(View.GONE);
+            alsoKnownAsTv.setVisibility(View.GONE);
         }
     }
 }
